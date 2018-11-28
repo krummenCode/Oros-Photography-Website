32379841a67c77b4183ce08ce4b2069c70ce4c0b
krummenCode <krummen.code@gmail.com>
2018-11-27 20:04:04 -0600

    Dark theme testing

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+//====
 // Animate Smooth Scroll
 
 //sets event on view work btn
 //function moves images to top of screen
-
 $('#view-work').on('click', function() {
 	//constant sets location of images to top
   const images = $('#images').position().top;
@@ -11,8 +11,26 @@ $('#view-work').on('click', function() {
     {
       scrollTop: images
     },
-	
+
 	//speed of scroll
-    1111 
+    1111
   );
 });
+
+
+//==========
+//Theme btn
+//=========
+
+//Dark theme function
+function darkThemeActivator() {
+  //toggle bodyDarkTheme class to body
+  var body = document.getElementById('body');
+  body.classList.toggle('bodyDarkTheme');
+
+  //toggle darkTheme class to main and #landing-text
+  var main = document.getElementById('main');
+  var landingText = document.getElementById('landing-text');
+  main.classList.toggle('darkTheme');
+  landingText.classList.toggle('darkTheme');
+}
